6d6c020856a3fba4c22f34628427aacdf5d604cd
Tung1409 <124113949+Tung1409@users.noreply.github.com>
2023-03-17 22:44:44 +0700

    Update CheckItemDetailsFlow.java

--- src/main/java/test_flows/authentication/CheckItemDetailsFlow.java
+++ src/main/java/test_flows/authentication/CheckItemDetailsFlow.java
@@ -12,53 +12,29 @@ public class CheckItemDetailsFlow extends BaseFlow {
     }
 
     public void verifyOmega3(){
-        HomePage homePage = new HomePage(appiumDriver);
-        homePage.searchAndClickOnItem("Omega");
         String expectedProductName = "Omega 3";
         String expectedProductBrand = "Natural";
         String expectedProductStatus = "Availability: Available";
         String expectedProductDes = "Good for heart";
         int expectedProductPrice = 14;
-
-        String actualProductName = homePage.getProductName();
-        String actualProductBrand = homePage.getProductBrand();
-        String actualProductStatus = homePage.getProductStatus();
-        String actualProductDes = homePage.getProductDescription();
-        int actualProductPrice = homePage.getProductPrice();
-
-        Assert.assertEquals(actualProductName, expectedProductName, "[ERR] Wrong product name");
-        Assert.assertEquals(actualProductBrand, expectedProductBrand, "[ERR] Wrong product brand");
-        Assert.assertEquals(actualProductStatus, expectedProductStatus, "[ERR] Wrong product status");
-        Assert.assertEquals(actualProductDes, expectedProductDes, "[ERR] Wrong product description");
-        Assert.assertEquals(actualProductPrice, expectedProductPrice, "[ERR] Wrong product price");
+        verifyProduct(expectedProductName, expectedProductBrand, expectedProductStatus,
+                expectedProductDes, expectedProductPrice);
     }
 
     public void verifyCleanCode(){
-        HomePage homePage = new HomePage(appiumDriver);
-        homePage.searchAndClickOnItem("Clean");
         String expectedProductName = "Clean code";
         String expectedProductBrand = "Book";
         String expectedProductStatus = "Availability: Available";
         String expectedProductDes = "Clean code";
         int expectedProductPrice = 35;
-
-        String actualProductName = homePage.getProductName();
-        String actualProductBrand = homePage.getProductBrand();
-        String actualProductStatus = homePage.getProductStatus();
-        String actualProductDes = homePage.getProductDescription();
-        int actualProductPrice = homePage.getProductPrice();
-
-        Assert.assertEquals(actualProductName, expectedProductName, "[ERR] Wrong product name");
-        Assert.assertEquals(actualProductBrand, expectedProductBrand, "[ERR] Wrong product brand");
-        Assert.assertEquals(actualProductStatus, expectedProductStatus, "[ERR] Wrong product status");
-        Assert.assertEquals(actualProductDes, expectedProductDes, "[ERR] Wrong product description");
-        Assert.assertEquals(actualProductPrice, expectedProductPrice, "[ERR] Wrong product price");
+        verifyProduct(expectedProductName, expectedProductBrand, expectedProductStatus,
+                expectedProductDes, expectedProductPrice);
     }
 
-    private void verifyProduct(String itemName, String expectedProductName, String expectedProductBrand,
+    private void verifyProduct( String expectedProductName, String expectedProductBrand,
                               String expectedProductStatus, String expectedProductDes, int expectedProductPrice){
         HomePage homePage = new HomePage(appiumDriver);
-        homePage.searchAndClickOnItem(itemName);
+        homePage.searchAndClickOnItem(expectedProductName);
         String actualProductName = homePage.getProductName();
         String actualProductBrand = homePage.getProductBrand();
         String actualProductStatus = homePage.getProductStatus();
